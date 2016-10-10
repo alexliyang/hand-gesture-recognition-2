c8863d18138111d69091e7546a44fb4003f4aeec
Xingjia Zhang <xzhang009@e.ntu.edu.sg>
2016-10-10 21:40:38 +0800

    added walk path; save as 150 * 150 images with same folder structur

--- preprocess.py
+++ preprocess.py
@@ -6,9 +6,7 @@ from matplotlib import cm
 from scipy.signal import medfilt as med_filter
 from scipy.ndimage.filters import gaussian_filter
 import cv2, os, sys
-
-sys.path.append('/usr/local/lib/python2.7/site-packages')
-
+import scipy.misc
 
 def normalize_to_gray_scale(old_array, min, max):
     new_array = np.zeros(old_array.shape)
@@ -17,14 +15,7 @@ def normalize_to_gray_scale(old_array, min, max):
             new_array[row_idx][elm_idx] = (elm - min) * 255 / float(max - min)
     return new_array
 
-
 def plot_overview(depth_array, grid, counter, image_per_row):
-    """
-	:param deptharray:
-	:param grid:
-	:param counter:
-	:param image_per_row:
-	"""
     sub = plt.subplot(grid[counter / image_per_row, counter % image_per_row])
     sub.axes.get_xaxis().set_visible(False)
     sub.axes.get_yaxis().set_visible(False)
@@ -32,7 +23,7 @@ def plot_overview(depth_array, grid, counter, image_per_row):
 
 
 def crop(depth_array, max_size):
-    _, threshold = cv2.threshold(depth_array.astype(np.uint8), 1, np.amax(depth_array), 0)
+    _, threshold = cv2.threshold(depth_array.copy().astype(np.uint8), 1, np.amax(depth_array), 0)
     points = cv2.findNonZero(threshold)
     x, y, w, h = cv2.boundingRect(points)
     # print (x,y,w,h)
@@ -49,18 +40,15 @@ def substract_background(depth_array, confarray, empty_pixel_val=1):
     depth_array = gaussian_filter(depth_array, 0.5)
 
     # remove low confidence and high dist pixels
-    max_dist = np.amax(depth_array);
     low_conf_ind = confarray < np.median(confarray) * 1.15
     high_dep_ind = depth_array > np.median(depth_array) * 0.85
     depth_array[low_conf_ind] = empty_pixel_val
     depth_array[high_dep_ind] = empty_pixel_val
-    depth_array = normalize_to_gray_scale(depth_array, np.amin(depth_array), max_dist)
 
     # smooth again
     depth_array = gaussian_filter(depth_array, 0.25)
     depth_array = med_filter(depth_array, 5)
     return depth_array
-
 
 def preprocess_images(rootdir, image_per_row):
     global confi_path
@@ -95,13 +83,68 @@ def preprocess_images(rootdir, image_per_row):
     plt.axis('off')
     plt.show()
 
+def preprocess_image(image_path, output_path):
+    confi_path = image_path.replace("depth_", "confi_")
+
+    im = Image.open(image_path)
+    conf = Image.open(confi_path)
+    depth_array = np.array(im)
+    confarray = np.array(conf)
+
+    depth_array = substract_background(depth_array, confarray)
+    max_dist = np.amax(im)
+    depth_array = normalize_to_gray_scale(depth_array, np.amin(depth_array), max_dist)
+    depth_array = crop(depth_array, 150)
+
+    scipy.misc.imsave(output_path, depth_array)
+
+def walk_data_folder(rootdir, output_dir="processed/"):
+    for subject_folder in os.listdir(rootdir):
+        subject_dir = os.path.join(rootdir, subject_folder)
+        if os.path.isdir(subject_dir):
+            walk_subject_folder(rootdir, subject_folder, output_dir)
+
+def walk_subject_folder(rootdir, subject_folder, output_dir="processed/"):
+    subject_dir = os.path.join(rootdir, subject_folder)
+    os.makedirs(os.path.join(output_dir, subject_folder))
+    for gesture_folder in os.listdir(subject_dir):
+        gesture_dir = os.path.join(rootdir, subject_folder, gesture_folder)
+        if os.path.isdir(gesture_dir):
+            walk_gesture_folder(rootdir, subject_folder, gesture_folder, output_dir="processed/")
+
+def walk_gesture_folder(rootdir, subject_folder, gesture_folder, output_dir="processed/"):
+    gesture_dir = os.path.join(rootdir, subject_folder, gesture_folder)
+    os.makedirs(os.path.join(output_dir, subject_folder, gesture_folder))
+    print "Reading folder", gesture_dir
+    for image_file in os.listdir(gesture_dir):
+        image_path = os.path.join(gesture_dir, image_file)
+        if os.path.isfile(image_path):
+            if "confi" in image_path or not image_path.endswith(".png"):
+                continue
+            image_output_path = os.path.join(output_dir, subject_folder, gesture_folder, image_file)
+            preprocess_image(image_path, image_output_path)
 
 if __name__ == "__main__":
-    rootdir = 'SSF/ssf14-{subject}-depth/{gesture}'
-    subject = sys.argv[1]
-    gesture_id = sys.argv[2]
-    image_per_row = sys.argv[3]
 
-    rootdir = rootdir.replace('{subject}', subject)
-    rootdir = rootdir.replace('{gesture}', gesture_id)
-    preprocess_images(rootdir, int(image_per_row))
+    rootdir = 'SSF/'
+    outputdir = 'processed/'
+
+    if not os.path.exists(outputdir):
+        os.makedirs(outputdir)
+    else:
+        print "Remove output folder before making a new one :)"
+        exit()
+
+    if (len(sys.argv) < 2):
+        print "Processing all images."
+        walk_data_folder(rootdir)
+
+    if (len(sys.argv) == 2):
+        print "Processing images for subject: ", sys.argv[1]
+        subject_folder = 'ssf14-' + sys.argv[1] + '-depth/'
+        walk_subject_folder(rootdir, subject_folder)
+
+    if (len(sys.argv) == 3):
+        print "processing images for subject:", sys.argv[1], "gesture:", sys.argv[2]
+        subject_folder = 'ssf14-' + sys.argv[1] + '-depth/'
+        walk_gesture_folder(rootdir, subject_folder, sys.argv[2])
