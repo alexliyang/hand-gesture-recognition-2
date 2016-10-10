complete implementation without buggy gradient function. not sure if cost function is correct either

nn.py
@@ -23,24 +23,24 @@ def nnCostFunction(theta, K, X, y, lmbda):
     cost = -1.0/m * (y * np.log(a[-1]) + (1 - y) * np.log(1 - a[-1])).sum()
     regCost = 0
     for k in range(0,K):
-        regCost += np.square(theta[k][:,2:]).sum()
+        regCost += np.square(theta[k][:,1:]).sum()
     regCost = lmbda/(2.0*m) * regCost    
     nnCost = cost + regCost
 
     # compute gradient
-    d = [a[-1] - y]
-    for k in range(1,K):
-        d.append(d[-1].dot(theta[-k])[:,1:] * sigmoidGradient(z[-(k+1)]))
-    d.reverse()
+    #d = [a[-1] - y]
+    #for k in range(1,K):
+    #    d.append(d[-1].dot(theta[-k])[:,1:] * sigmoidGradient(z[-(k+1)]))
+    #d.reverse()
 
-    thetaGrad = np.array([])
-    for k in range(0,K):
-        regGrad = np.append(np.zeros((theta[k].shape[0],1)), lmbda/m * theta[k][:,1:], axis=1)
-        thetaGrad = np.append(thetaGrad, 1.0/m * d[k].transpose().dot(a[k]) + regGrad)
+    #thetaGrad = np.array([])
+    #for k in range(0,K):
+    #    regGrad = np.append(np.zeros((theta[k].shape[0],1)), lmbda/m * theta[k][:,1:], axis=1)
+    #    thetaGrad = np.append(thetaGrad, 1.0/m * d[k].transpose().dot(a[k]) + regGrad)
 
-    return (nnCost, thetaGrad)
+    return nnCost #(nnCost, thetaGrad)    
 
-def nnCostFunctionFlattened(theta_flat, layerSizes, X, y, lmbda):
+def reshapeTheta(theta_flat, layerSizes):
     K = len(layerSizes) - 1
     theta = []
     thetaStart = 0
@@ -48,23 +48,40 @@ def nnCostFunctionFlattened(theta_flat, layerSizes, X, y, lmbda):
         thetaEnd = thetaStart + layerSizes[k+1] * (layerSizes[k]+1)
         theta.append(np.reshape(theta_flat[thetaStart:thetaEnd], (layerSizes[k+1], (layerSizes[k]+1))))
         thetaStart = thetaEnd
-    
-    (cost, thetaGrad) = nnCostFunction(theta, K, X, y, lmbda)
-    return (cost, thetaGrad)
+    return theta
 
-def backPropagation(layerSizes, K, X, y, lmbda):
-    # init randomize thetas
-    # call fmin_cg passing nnCostFunctionFlattened
-    # return trained model
-    return 0
+def nnCostFunctionFlattened(theta_flat, layerSizes, X, y, lmbda):
+    K = len(layerSizes) - 1
+    theta = reshapeTheta(theta_flat, layerSizes)
+    cost = nnCostFunction(theta, K, X, y, lmbda)
+    return cost
+
+def backPropagation(layerSizes, X, y, lmbda):
+    args = (layerSizes, X, y, lmbda)
+    def f(x, *args):
+        layerSizes, X, y, lmbda = args
+        return nnCostFunctionFlattened(x, layerSizes, X, y, lmbda)
+
+    initRange = 0.24
+    theta = np.array([])
+    K = len(layerSizes) - 1
+    for k in range(0,K):
+        theta = np.append(theta, np.random.rand(layerSizes[k+1], layerSizes[k]+1) * initRange)
+
+    # optimize and return
+    thetaOpt = fmin_cg(f, theta, maxiter=400, args=args)
+    return reshapeTheta(thetaOpt, layerSizes)
 
 def infer(input, theta):
     # forward propagation
-    return 0
+    inference = np.array(input)
+    for k in range(0,len(theta)):
+        inference = np.append(1, inference).dot(theta[k].transpose())
+    return np.argmax(inference)
 
 def debugInitialWeights(nIn, nOut):
     W = np.zeros((nOut, nIn + 1))
-    return np.reshape(np.sin(range(1,W.size+1)), W.shape, 'F') / 10.0
+    return np.reshape(np.sin(range(1,W.size+1)), W.shape) / 10.0
 
 def numericalGradient(theta, X, y, lmbda):
     numgrad = np.zeros(theta.size)
@@ -104,15 +121,12 @@ def debug():
 
     (cost, grad) = nnCostFunctionFlattened(nn_params, [3,5,3], X, np.array(y_encoded), 0)
     numgrad = numericalGradient(nn_params, X, np.array(y_encoded), 0)
-
-    print cost
-    print cost.shape
-    print "\n"
     print grad
-    print grad.shape
-    print "\n"
     print numgrad
-    print numgrad.shape
 
 if __name__ == "__main__":
-    debug()
+    theta = backPropagation([2,4,4,2], np.array([[0,0],[0,1],[1,0],[1,1],[0,0],[0,1],[1,0],[1,1]]), np.array([[1,0],[0,1],[0,1],[1,0],[1,0],[0,1],[0,1],[1,0]]), 0)
+    print infer([0,0], theta)
+    print infer([0,1], theta)
+    print infer([1,0], theta)
+    print infer([1,1], theta)