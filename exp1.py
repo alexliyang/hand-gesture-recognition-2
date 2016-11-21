fafcbb38e7f7df2ef4e6d900ea71e1b2993daed1
Arnold Koroa <arnold.koroa@gmail.com>
2016-11-22 00:15:24 +0800

    commit final changes

--- exp1.py
+++ exp1.py
@@ -15,24 +15,36 @@ def testModel(model, X, y, nGesture):
     for i in range(0,X.shape[0]):
         guess = nn.infer(X[i], model)
         nGuess[guess] = nGuess[guess] + 1
-        print "Guess", i, ":", guess, "Actual :", np.argmax(y[i])
+        #print "Guess", i, ":", guess, "Actual :", np.argmax(y[i])
         nCorrect = nCorrect + y[i][guess]
-    print "nGuesses :", nGuess
-    print nCorrect, "correct out of", X.shape[0]
+    #print nCorrect, "correct out of", X.shape[0]
     print "Accuracy :", nCorrect * 1.0 / X.shape[0] * 100.0, "%"
-
+    print "nGuesses :", nGuess
+    
 if __name__ == "__main__":
     imageReader = ImageReader()
     nHiddenLayer = 3
     hiddenLayerSize = 1000
     lmbda = 0.01
-    maxIter = 40
+    maxIter = 400
 
     hiddenLayerSizes = []
     for i in range(0, nHiddenLayer):
         hiddenLayerSizes.append(hiddenLayerSize)
     layerSizes = [imageReader.n] + hiddenLayerSizes + [imageReader.nGesture]
     
-    (X,y) = imageReader.readImages()
+    (X,y), (Xv, yv) = imageReader.readImages()
+    print len(X), "training data"
+    print len(Xv), "test data"
     model = trainModel(layerSizes, X, y, lmbda, maxIter)
+    
+    print "nHiddenLayer :", nHiddenLayer
+    print "hiddenLayerSize :", hiddenLayerSize
+    print "lmbda :", lmbda
+    print "maxIter :", maxIter
+
+    print "Training",
     testModel(model, X, y, imageReader.nGesture)
+
+    print "Test",
+    testModel(model, Xv, yv, imageReader.nGesture)
