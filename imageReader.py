21c48b80e0aa7905420a1afb9f69f9e9857d5bdf
Arnold Koroa <arnold.koroa@gmail.com>
2016-11-23 09:11:34 +0800

    push extract feature for feed-forward nn

--- imageReader.py
+++ imageReader.py
@@ -1,6 +1,27 @@
 import os, sys
 import numpy as np
 from PIL import Image
+from skimage.feature import hog
+import matplotlib.pyplot as plt
+
+def extract_hog_feature(input_img_path):
+    # input_img_path = 'testPCA.png'
+
+    image = Image.open(input_img_path)
+
+    depth_array = np.array(image)
+
+    fd = hog(image, orientations=360, pixels_per_cell=(16, 16),
+                        cells_per_block=(1, 1), visualise=False)
+    # print len(fd)
+    threshold = np.mean(fd) * 1.5
+    # print threshold
+    fd = np.reshape(fd, (360, 36))
+    # print fd.shape
+    fd = np.where(fd > threshold, fd, 0)
+
+    fd = np.sum(fd, axis=1)
+    return fd
 
 class ImageReader:
     def __init__(self):
@@ -11,9 +32,11 @@ class ImageReader:
     def readImages(self, make1d=True, validationRatio=0.1):
         xs = []
         ys = []
+        print "root is:", self.rootDir
 
         iGesture = 0
         for gestureFolder in os.listdir(self.rootDir):
+            print "Processing", gestureFolder
             gestureDir = os.path.join(self.rootDir, gestureFolder)
             if not os.path.isdir(gestureDir):
                 continue
@@ -26,9 +49,10 @@ class ImageReader:
                 if not os.path.isfile(imagePath) or not imagePath.endswith(".png"):
                     continue
 
-                image = np.array(Image.open(imagePath))
+                # image = np.array(Image.open(imagePath))
                 if make1d: 
-                    image = image.flatten()
+                    image = extract_hog_feature(imagePath)
+                    self.n = 360
                 else:
                     image = [image]
                 xs.append(image)
